9b4a831aed54bacf892fd3dbf6c24946752b0111
Luis Pena <luisp.buzon@gmail.com>
2020-03-27 14:18:16 +0100

    Error al crear y al validar el email

--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,32 +1,35 @@
-import React from 'react'
-import { StyleSheet,View,Image,Text } from "react-native"
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
-import RegisterForm from "../../components/Account/RegisterForm"
+import React, { useRef } from "react";
+import { StyleSheet, View, Image, Text } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import RegisterForm from "../../components/Account/RegisterForm";
+import Toast from "react-native-easy-toast";
 
 export default function Register() {
-    
-    return (
-        <KeyboardAwareScrollView>
-            <Image
-                source={require("../../../assets/img/logo.png")}
-                style={styles.logo}
-            resizeMode="contain"/>
-            <View style={styles.viewForm}>
-                <RegisterForm/>
-            </View>
-        </KeyboardAwareScrollView>
-    )
+  const toastRef = useRef();
+
+  return (
+    <KeyboardAwareScrollView>
+      <Image
+        source={require("../../../assets/img/logo.png")}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <View style={styles.viewForm}>
+        <RegisterForm toastRef={toastRef} />
+      </View>
+      <Toast ref={toastRef} position="center" opacity={0.5} />
+    </KeyboardAwareScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-
-    logo: {
-        width:"100%",
-        height:150,
-        marginTop:20
-    },
-    viewForm: {
-        marginRight: 40,
-        marginLeft:40
-    }
-})
+  logo: {
+    width: "100%",
+    height: 150,
+    marginTop: 20
+  },
+  viewForm: {
+    marginRight: 40,
+    marginLeft: 40
+  }
+});
